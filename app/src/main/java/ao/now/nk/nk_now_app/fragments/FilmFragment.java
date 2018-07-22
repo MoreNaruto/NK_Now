9b1f46ed9d7ca4d0dbf7529b37a7a5d9853fedac
Thomas Morris <morenaruto@Thomass-MacBook-Pro.local>
2018-07-22 18:32:37 -0500

    Redesigned films page and allow links for nk live, nk link, and original films

--- app/src/main/java/ao/now/nk/nk_now_app/fragments/FilmFragment.java
+++ app/src/main/java/ao/now/nk/nk_now_app/fragments/FilmFragment.java
@@ -1,5 +1,9 @@
 package ao.now.nk.nk_now_app.fragments;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -12,16 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ao.now.nk.nk_now_app.R;
+import ao.now.nk.nk_now_app.databinding.FragmentFilmPageBinding;
 import it.chengdazhi.decentbanner.DecentBanner;
 
 public class FilmFragment extends Fragment {
     public static final String TAG = FilmFragment.class.getSimpleName();
-    private DecentBanner newFilmsBanner;
-    private DecentBanner trendingFilmsBanner;
-    private List<View> newFilmsBannerListViews;
-    private List<View> trendingFilmsBannerListViews;
-    private List<String> newFilmsBannerListTitles;
-    private List<String> trendingFilmsBannerListTitles;
+    private FragmentFilmPageBinding binding;
 
     public static FilmFragment newInstance() {
         Bundle args = new Bundle();
@@ -39,42 +39,48 @@ public class FilmFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_film_page, container, false);
-        
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_film_page, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        newFilmsBanner = getActivity().findViewById(R.id.new_films_banner);
-        trendingFilmsBanner = getActivity().findViewById(R.id.trending_films_banner);
-
         View featuredFilmOne = getActivity().getLayoutInflater().inflate(R.layout.featured_film_1, null);
         View featuredFilmTwo = getActivity().getLayoutInflater().inflate(R.layout.featured_film_2, null);
         View featuredFilmThree = getActivity().getLayoutInflater().inflate(R.layout.featured_film_3, null);
         View featuredFilmFour = getActivity().getLayoutInflater().inflate(R.layout.featured_film_4, null);
         View featuredFilmFive = getActivity().getLayoutInflater().inflate(R.layout.featured_film_5, null);
 
-        newFilmsBannerListViews = new ArrayList<>();
+        List<View> newFilmsBannerListViews = new ArrayList<>();
         newFilmsBannerListViews.add(featuredFilmOne);
         newFilmsBannerListViews.add(featuredFilmTwo);
 
-
-        trendingFilmsBannerListViews = new ArrayList<>();
+        List<View> trendingFilmsBannerListViews = new ArrayList<>();
         trendingFilmsBannerListViews.add(featuredFilmThree);
         trendingFilmsBannerListViews.add(featuredFilmFour);
         trendingFilmsBannerListViews.add(featuredFilmFive);
 
-        newFilmsBannerListTitles = new ArrayList<>();
+        List<String> newFilmsBannerListTitles = new ArrayList<>();
         newFilmsBannerListTitles.add(".");
         newFilmsBannerListTitles.add(".");
 
-        trendingFilmsBannerListTitles = new ArrayList<>();
+        List<String> trendingFilmsBannerListTitles = new ArrayList<>();
         trendingFilmsBannerListTitles.add("Film Access");
         trendingFilmsBannerListTitles.add("Sports");
         trendingFilmsBannerListTitles.add("Music");
 
-        newFilmsBanner.start(newFilmsBannerListViews, newFilmsBannerListTitles, 2, 500, R.drawable.ic_launcher);
-        trendingFilmsBanner.start(trendingFilmsBannerListViews, trendingFilmsBannerListTitles, 2, 500, R.drawable.ic_launcher);
+        binding.newFilmsBanner.start(newFilmsBannerListViews, newFilmsBannerListTitles, 2, 500, R.drawable.ic_launcher);
+        binding.trendingFilmsBanner.start(trendingFilmsBannerListViews, trendingFilmsBannerListTitles, 2, 500, R.drawable.ic_launcher);
+
+        binding.originalFilmsImageView.setOnClickListener(v -> goToUrl("https://www.newkingstonfilm.com/nk-films"));
+        binding.nkLinkImageView.setOnClickListener(v -> goToUrl("https://www.newkingstonfilm.com/nk-link"));
+        binding.nkLiveImageView.setOnClickListener(v -> goToUrl("https://www.newkingstonfilm.com"));
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void goToUrl(String url) {
+        Uri parsedUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, parsedUrl);
+        startActivity(launchBrowser);
     }
 }
